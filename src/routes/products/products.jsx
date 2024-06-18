12ed9fb7c99e73f5b9042bb1bfdfee2b6979b2c7
Shamaem Saqib <shamaem_saqib@hotmail.com>
2024-06-18 14:35:10 +0500

    Added filters functionality to products page

--- src/routes/products/products.jsx
+++ src/routes/products/products.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./products.css";
-import { products } from "../../utils/data.js";
+import allProductsArr, {
+  riceProducts,
+  saltProducts,
+  spiceProducts,
+} from "../../utils/data.js";
 
 import FilterIcn from "../../assets/icons/filter-solid.svg";
 
@@ -9,7 +13,51 @@ import BoldLightTitle from "../../Components/boldLightTitle/boldLightTitle.jsx";
 import SingleProductCard from "../../Components/singleProductCard/singleProductCard.jsx";
 
 function Products() {
-  console.log(products);
+  const [currProducts, setCurrProducts] = useState(allProductsArr);
+  const [, setCurrSearchText] = useState("");
+  const [currCategory, setCurrCategory] = useState(0);
+
+  const filterCategoryClickHandler = (event) => {
+    const selectedCategory = event.target.innerText.toLowerCase();
+    switch (selectedCategory) {
+      case "all":
+        setCurrProducts(allProductsArr);
+        setCurrCategory(0);
+        break;
+      case "rice":
+        setCurrProducts(riceProducts);
+        setCurrCategory(1);
+        break;
+      case "pink salt":
+        setCurrProducts(saltProducts);
+        setCurrCategory(2);
+        break;
+      case "spices":
+        setCurrProducts(spiceProducts);
+        setCurrCategory(3);
+        break;
+      default:
+        break;
+    }
+    window.scrollTo({
+      top:
+        document.getElementById("products-display-subsection-wrapper")
+          .offsetTop -
+        window.innerHeight * 0.14,
+    });
+  };
+
+  const searchChangeHandler = (event) => {
+    const currText = event.target.value;
+    setCurrSearchText(currText);
+    setCurrCategory(0);
+    setCurrProducts(
+      allProductsArr.filter((product) =>
+        product.name.toLowerCase().includes(currText.toLowerCase())
+      )
+    );
+  };
+
   return (
     <section id="products-section-wrapper">
       <div id="products-section">
@@ -23,33 +71,65 @@ function Products() {
           type="text"
           id="products-section-searchbar"
           placeholder="search a product"
+          onChange={searchChangeHandler}
         />
         <div id="products-display-subsection-wrapper">
           <div id="products-display-subsection-filter-wrapper">
-            <h4 id="products-display-subsection-filter-title">
-              <img src={FilterIcn} alt="" /> filter
-            </h4>
-            <div className="products-display-subsection-filter-single-section">
-              <h4 className="products-display-subsection-filter-single-section-title">
-                category
+            <div id="products-display-subsection-filter">
+              <h4 id="products-display-subsection-filter-title">
+                <img src={FilterIcn} alt="" /> filter
               </h4>
-              <ul className="products-display-subsection-filter-single-section-list">
-                <li>all</li>
-                <li>rice</li>
-                <li id="active">pink salt</li>
-                <li>spices</li>
-              </ul>
+              <div className="products-display-subsection-filter-single-section">
+                <h4 className="products-display-subsection-filter-single-section-title">
+                  category
+                </h4>
+                <ul className="products-display-subsection-filter-single-section-list">
+                  <li
+                    onClick={filterCategoryClickHandler}
+                    id={`${currCategory === 0 ? `active` : ``}`}
+                  >
+                    all
+                  </li>
+                  <li
+                    onClick={filterCategoryClickHandler}
+                    id={`${currCategory === 1 ? `active` : ``}`}
+                  >
+                    rice
+                  </li>
+                  <li
+                    onClick={filterCategoryClickHandler}
+                    id={`${currCategory === 2 ? `active` : ``}`}
+                  >
+                    pink salt
+                  </li>
+                  <li
+                    onClick={filterCategoryClickHandler}
+                    id={`${currCategory === 3 ? `active` : ``}`}
+                  >
+                    spices
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
           <div id="products-display-subsection-products-wrapper">
-            <h4 id="products-display-subsection-products-count">
-              1 products found <hr />
-            </h4>
-            <div id="products-display-subsection-product-cards-wrapper">
-              {products.map((product) => (
-                <SingleProductCard product={product} />
-              ))}
-            </div>
+            {currProducts.length > 0 ? (
+              <div>
+                <h4 id="products-display-subsection-products-count">
+                  <hr />
+                  {currProducts.length} products found <hr />
+                </h4>
+                <div id="products-display-subsection-product-cards-wrapper">
+                  {currProducts.map((product) => (
+                    <SingleProductCard product={product} />
+                  ))}
+                </div>
+              </div>
+            ) : (
+              <h4 id="products-display-subsection-no-products">
+                No products matched your search!
+              </h4>
+            )}
           </div>
         </div>
       </div>
--- src/utils/data.js
+++ src/utils/data.js
@@ -1,57 +1,137 @@
-export const products = [
+export const riceProducts = [
   {
     name: "Super Kernel Basmati",
-    image: require("../assets/products/1121-White-Super-Basmati-Rice-300x300.jpg"),
+    image: require("../assets/products/rice/1121-White-Super-Basmati-Rice-300x300.jpg"),
     category: "rice",
   },
   {
     name: "IRRI-6 White Rice",
-    image: require("../assets/products/IRRI-6-Brown-Rice-300x300.jpg"),
+    image: require("../assets/products/rice/IRRI-6-Brown-Rice-300x300.jpg"),
     category: "rice",
   },
   {
     name: "1121 Steam Basmati Rice",
-    image: require("../assets/products/1121_Steam_Basmati_Rice-300x300.jpg"),
+    image: require("../assets/products/rice/1121_Steam_Basmati_Rice-300x300.jpg"),
     category: "rice",
   },
   {
     name: "1121 Parboiled Rice",
-    image: require("../assets/products/golden-sella-basmati-rice-500x500-300x300.jpg"),
+    image: require("../assets/products/rice/golden-sella-basmati-rice-500x500-300x300.jpg"),
     category: "rice",
   },
   {
     name: "PK-386 White Rice",
-    image: require("../assets/products/386-300x300.jpg"),
+    image: require("../assets/products/rice/386-300x300.jpg"),
     category: "rice",
   },
   {
     name: "1121 Parboiled Basmati Rice",
-    image: require("../assets/products/1121-Parboiled-Basmati-Rice-300x300.jpeg"),
+    image: require("../assets/products/rice/1121-Parboiled-Basmati-Rice-300x300.jpeg"),
     category: "rice",
   },
   {
     name: "1121 White Super Basmati Rice",
-    image: require("../assets/products/1121-White-Super-Basmati-Rice-300x300.jpg"),
+    image: require("../assets/products/rice/1121-White-Super-Basmati-Rice-300x300.jpg"),
     category: "rice",
   },
   {
     name: "C-9 Brown Rice",
-    image: require("../assets/products/C-9-Brown-Rice.jpg"),
+    image: require("../assets/products/rice/C-9-Brown-Rice.jpg"),
     category: "rice",
   },
   {
     name: "IRRI-6 Brown Rice",
-    image: require("../assets/products/IRRI-6-Brown-Rice-300x300.jpg"),
+    image: require("../assets/products/rice/IRRI-6-Brown-Rice-300x300.jpg"),
     category: "rice",
   },
   {
     name: "Super Kernel Basmati Brown Rice",
-    image: require("../assets/products/Super-Kernel-Basmati-Brown-Rice-300x300.jpg"),
+    image: require("../assets/products/rice/Super-Kernel-Basmati-Brown-Rice-300x300.jpg"),
     category: "rice",
   },
   {
     name: "Super Kernel Basmati Parboiled Brown Rice",
-    image: require("../assets/products/Super-Kernel-Basmati-Parboiled-Brown-Rice-300x300.jpg"),
+    image: require("../assets/products/rice/Super-Kernel-Basmati-Parboiled-Brown-Rice-300x300.jpg"),
     category: "rice",
   },
-];
+].sort((a, b) => a.name.localeCompare(b.name));
+
+export const saltProducts = [
+  {
+    name: "Figure Shape",
+    image: require("../assets/products/salt/Figure Shape.jpg"),
+    category: "salt",
+  },
+  {
+    name: "Fire Bowl",
+    image: require("../assets/products/salt/fire-bawls-300x300.png"),
+    category: "salt",
+  },
+  {
+    name: "Geometrical Shapes",
+    image: require("../assets/products/salt/Geometrical Shapes 300x300.jpg"),
+    category: "salt",
+  },
+  {
+    name: "Iron Bowls",
+    image: require("../assets/products/salt/Iron Bowls-300x300.jpg"),
+    category: "salt",
+  },
+  {
+    name: "Night Lamps",
+    image: require("../assets/products/salt/Night Lamps.jpg"),
+    category: "salt",
+  },
+  {
+    name: "Salt Inhalers",
+    image: require("../assets/products/salt/Salt Inhalers-300x300.jpg"),
+    category: "salt",
+  },
+  {
+    name: "USB Lamps",
+    image: require("../assets/products/salt/usb-lamps-300x300.png"),
+    category: "salt",
+  },
+  {
+    name: "Natural Shapes",
+    image: require("../assets/products/salt/natural-shape.png"),
+    category: "salt",
+  },
+].sort((a, b) => a.name.localeCompare(b.name));
+
+export const spiceProducts = [
+  {
+    name: "Coriander Powder",
+    image: require("../assets/products/spice/Coriander-Powder-1-300x300.jpg"),
+    category: "spice",
+  },
+  {
+    name: "Garlic Powder",
+    image: require("../assets/products/spice/Garlic-powder-300x300.jpg"),
+    category: "spice",
+  },
+  {
+    name: "Red Chilli Flakes",
+    image: require("../assets/products/spice/Red-Chilli-Flakes-300x300.jpg"),
+    category: "spice",
+  },
+  {
+    name: "Turmeric Powder",
+    image: require("../assets/products/spice/turmeric-powder-300x300.jpg"),
+    category: "spice",
+  },
+  {
+    name: "Black Pepper",
+    image: require("../assets/products/spice/coffee-500x500-300x300.jpg"),
+    category: "spice",
+  },
+  {
+    name: "Red Chilli Powder",
+    image: require("../assets/products/spice/red-chilli-powder-300x300.png"),
+    category: "spice",
+  },
+].sort((a, b) => a.name.localeCompare(b.name));
+
+let allProductsArr = riceProducts.concat(saltProducts, spiceProducts);
+
+export default allProductsArr.sort((a, b) => a.name.localeCompare(b.name));
